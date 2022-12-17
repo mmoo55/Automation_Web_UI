c067603deeecc49f5e934790c1bfc49f63efe775
mmoo55 <g.gerson51@gmail.com>
2022-12-17 12:36:52 -0400

    Fixing bugs

--- src/test/java/browser/Chrome.java
+++ src/test/java/browser/Chrome.java
@@ -1,7 +1,9 @@
 package browser;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 
@@ -10,13 +12,25 @@ public class Chrome implements IBrowser {
     @Override
     public WebDriver create() {
         /*System.setProperty("webdriver.chrome.driver","src/test/resources/driver/chromedriver.exe");*/
-        System.setProperty("webdriver.chrome.driver","src//test//resources//driver//chromedriver");
+        /*System.setProperty("webdriver.chrome.driver","src/test/resources/driver/chromedriver");*/
 
-        WebDriver driver = new ChromeDriver();
+        /*WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(15));
 
-        driver.manage().window().maximize();
+        driver.manage().window().maximize();*/
+
+        WebDriver driver = null;
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        options.addArguments("enable-automation");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-infobars");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-browser-side-navigation");
+        options.addArguments("--disable-gpu");
+        driver = new ChromeDriver(options);
 
         return driver;
     }
